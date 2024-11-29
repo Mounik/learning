3db3440e7be86e49cad359ff2e271b8881f65c10
Mounik <mounicou@gmail.com>
2024-11-29 15:56:45 +0100

    ✅ Ajout de src pour l'exercice de tri

---
+++ src/main.py
@@ -0,0 +1,42 @@
+"""
+Trier les fichiers contenus dans le dossier data selon les associations suivantes :
+mp3, wav, flac : Musique
+avi, mp4, gif : Videos
+bmp, png, jpg : Images
+txt, pptx, csv, xls, odp, pages : Documents
+autres : Divers
+"""
+from pathlib import Path
+
+dirs = {
+        ".mp3": "Musique",
+        ".wav": "Musique",
+        ".flac": "Musique",
+        ".avi": "Videos",
+        ".mp4": "Videos",
+        ".gif": "Videos",
+        ".bmp": "Images",
+        ".png": "Images",
+        ".jpg": "Images",
+        ".txt": "Documents",
+        ".pptx": "Documents",
+        ".csv": "Documents",
+        ".xls": "Documents",
+        ".odp": "Archives",
+        ".pages": "Documents",
+        }
+
+# On désigne le dossier a trier
+tri_dir = Path.home() / "python" / "learning" / "src" / "data"
+# Compréhension de liste pour obtenir la liste des fichiers du dossier a trier
+files = [f for f in tri_dir.iterdir() if f.is_file()]
+# On parcourt chaque fichier et on déplace le fichier dans le bon dossier en fonction de l'extension du fichier.
+# Si l'extension n'est pas dans la liste, on la place dans le dossier "Divers".
+for f in files:
+    # On crée le chemin du dossier de destination en fonction de l'extension du fichier.
+    # Si l'extension n'est pas dans la liste, on la place dans le dossier "Divers".
+    output_dir = tri_dir / dirs.get(f.suffix, "Divers")
+    # On crée le dossier de destination si il n'existe pas. Si le dossier existe déjà, on ne fait rien.
+    output_dir.mkdir(exist_ok=True)
+    # On déplace le fichier dans le bon dossier en fonction de l'extension du fichier.
+    f.rename(output_dir / f.name)
